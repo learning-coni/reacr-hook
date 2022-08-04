6013aa7175f77ca6da3d9c8e66e3fdc399a0ebec
unknown <kopiuy678@hotmail.com>
2022-08-04 13:02:52 +0900

    'company'

---
+++ src/redux/first.js
@@ -0,0 +1,54 @@
+const createPolicy = (name, amount) => {
+  return {
+    type: "CREATE_POLICY",
+    payload: {
+      name,
+      amount,
+    },
+  };
+};
+
+const deletePolicy = (name) => {
+  return {
+    type: "DELETE_POLICY",
+    payload: {
+      name,
+    },
+  };
+};
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+  return {
+    type: "CREATE_CLAIM",
+    payload: {
+      name,
+      amountOfMoneyToCollect,
+    },
+  };
+};
+
+// Reducers
+const claimHistory = (oldListOfClaims = [], action) => {
+  if (action.type === "CREATE_CLAIM") {
+    return [...oldListOfClaims, action.payload];
+  }
+  return oldListOfClaims;
+};
+
+const accounting = (bagOfMoney = 100, action) => {
+  if (action.type === "CREATE_CLAIM") {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if (action.type === "CREATE_POLICY") {
+    return bagOfMoney + action.payload.amount;
+  }
+  return bagOfMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+  if (action.type === "CREATE_POLICY") {
+    return [...listOfPolicies, action.payload.name];
+  } else if (action.type === "DELETE_POLICY") {
+    return listOfPolicies.filter((user) => user !== action.payload.name);
+  }
+  return listOfPolicies;
+};
